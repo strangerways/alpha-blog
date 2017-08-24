af308253064ec606818fc9f6231c50604e28fc32
John <john.parkinson@covenanteyes.com>
2017-08-24 14:34:13 +0000

    fixed flash messages

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,9 +5,20 @@ class ArticlesController < ApplicationController
   
   def create
    
-   @article = Article.new(article_params)
-   @article.save
-   redirect_to articles_show(@article)
+  @article = Article.new(article_params)
+   if @article.save
+   flash[:notice] = "Article was Saved"
+     redirect_to article_path(@article)
+   else
+     render 'new'
+   end
+   
+   
+  end
+  
+  def show
+    @article = Article.find(params[:id])
+    
   end
   
   private
