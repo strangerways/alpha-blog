aa3dbb9d5d8e713c7df207c26122d4333b6255d1
John <john.parkinson@covenanteyes.com>
2017-08-25 14:13:32 +0000

    added add/edit/inxed/show

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,18 @@
 class ArticlesController < ApplicationController
+  
+  def index
+    @articles = Article.all
+  end
+  
   def new
   @article = Article.new
   end
+  
+  def edit
+    @article = Article.find(params[:id])
+  
+  end
+  
   
   def create
    
@@ -16,6 +27,17 @@ class ArticlesController < ApplicationController
    
   end
   
+  def update
+    
+    @article = Article.find(params[:id]) 
+    if @article.update(article_params)
+      flash[:notice] = "Article was successfully updated"
+      redirect_to article_path(@article)
+    else
+        render 'edit'
+    end
+  end
+  
   def show
     @article = Article.find(params[:id])
     
@@ -26,5 +48,6 @@ class ArticlesController < ApplicationController
   params.require(:article).permit(:title, :description)
   
   end
-  
 end
+
+  
