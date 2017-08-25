527010ccfcf5c31e7d6840a9c5e20df126d90f46
John <john.parkinson@covenanteyes.com>
2017-08-25 14:28:09 +0000

    partials and destroy

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -38,6 +38,13 @@ class ArticlesController < ApplicationController
     end
   end
   
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    flash[:notice] = "Article Killed"
+    redirect_to articles_path
+  end
+  
   def show
     @article = Article.find(params[:id])
     
