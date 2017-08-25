83ff69a3fdd2eb27cc7083e098b6b13c729207e5
John <john.parkinson@covenanteyes.com>
2017-08-25 18:17:34 +0000

    style flash messages

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -18,7 +18,7 @@ class ArticlesController < ApplicationController
   def create
   @article = Article.new(article_params)
    if @article.save
-   flash[:notice] = "Article was Saved"
+   flash[:success] = "Article was Saved"
      redirect_to article_path(@article)
    else
      render 'new'
@@ -27,7 +27,7 @@ class ArticlesController < ApplicationController
   
   def update
     if @article.update(article_params)
-      flash[:notice] = "Article was successfully updated"
+      flash[:success] = "Article was successfully updated"
       redirect_to article_path(@article)
     else
         render 'edit'
@@ -36,7 +36,7 @@ class ArticlesController < ApplicationController
   
   def destroy
     @article.destroy
-    flash[:notice] = "Article Killed"
+    flash[:danger] = "Article Killed"
     redirect_to articles_path
   end
   
