0c2dc2ea82f252d5d3c7d6e9bb4ef84c57c3f12b
John <john.parkinson@covenanteyes.com>
2017-08-25 14:37:07 +0000

    prep for production

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,6 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: [:edit, :update, :show, :destroy]
+  
   
   def index
     @articles = Article.all
@@ -9,13 +11,11 @@ class ArticlesController < ApplicationController
   end
   
   def edit
-    @article = Article.find(params[:id])
-  
+   
   end
   
   
   def create
-   
   @article = Article.new(article_params)
    if @article.save
    flash[:notice] = "Article was Saved"
@@ -23,13 +23,9 @@ class ArticlesController < ApplicationController
    else
      render 'new'
    end
-   
-   
   end
   
   def update
-    
-    @article = Article.find(params[:id]) 
     if @article.update(article_params)
       flash[:notice] = "Article was successfully updated"
       redirect_to article_path(@article)
@@ -39,22 +35,27 @@ class ArticlesController < ApplicationController
   end
   
   def destroy
-    @article = Article.find(params[:id])
     @article.destroy
     flash[:notice] = "Article Killed"
     redirect_to articles_path
   end
   
   def show
-    @article = Article.find(params[:id])
     
   end
   
-  private
+  
+  private #private stuff starts here
+  
   def article_params
   params.require(:article).permit(:title, :description)
   
   end
+  
+  def set_article
+    @article = Article.find(params[:id])
+  end
+
 end
 
   
