b79601fbce8b71de92784d696c048b7ec724aaba
John <john.parkinson@covenanteyes.com>
2017-08-29 18:06:42 +0000

    added article asc

--- test/integration/list_categories_test.rb
+++ test/integration/list_categories_test.rb
@@ -1,17 +1,25 @@
 require 'test_helper'
 
-class CreateCategoriesTest < ActionDispatch::IntegrationTest
-  
-  def setup
-   @category = Category.create(name: "books")
-   @category2 = Category.create(nam: "programming")
-  end
-  
-  test "should show cats" do
-    get categories_path 
-    assert_template 'categories/index'
-    assert_select "a[href=?]", category_path(@category), text: @category.name
-    assert_select "a[href=?]", category_path(@category2), text: @category2.name
-  end
+class ListCategoriesTest < ActionDispatch::IntegrationTest
+
+def setup
+
+@category = Category.create(name: "sports")
+
+@category2 = Category.create(name: "programming")
+
+end
+
+test "should show categories listing" do
+
+get categories_path
+
+assert_template 'categories/index'
+
+assert_select "a[href=?]", category_path(@category), text: @category.name
+
+assert_select "a[href=?]", category_path(@category2), text: @category2.name
+
+end
 
 end
